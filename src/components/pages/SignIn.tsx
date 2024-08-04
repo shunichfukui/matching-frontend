f653d523df92d64a4124d62b19248cbfa08c00d3
shunich <fukusyun3@gmail.com>
2024-08-04 20:40:06 +0900

    fix: sign in sign up

--- src/components/pages/SignIn.tsx
+++ src/components/pages/SignIn.tsx
@@ -1,25 +1,22 @@
 import React, { useState, useContext } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
 import { makeStyles, Theme } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import Button from '@material-ui/core/Button';
-import Box from '@material-ui/core/Box';
+import { Typography, Card, CardContent, CardHeader, Button, Box } from '@material-ui/core';
 
 import { AuthContext } from 'App';
 import AlertMessage from 'components/utils/AlertMessage';
+import CommonFormInput from 'components/CommonFormInput';
 import { signIn } from 'lib/api/auth';
 import { SignInData } from 'interfaces/index';
 
 const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    marginTop: theme.spacing(6),
+  },
   submitBtn: {
-    paddingTop: theme.spacing(2),
-    textAlign: 'right',
+    marginTop: theme.spacing(2),
     flexGrow: 1,
     textTransform: 'none',
   },
@@ -28,20 +25,19 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   card: {
     padding: theme.spacing(2),
-    maxWidth: 400,
+    maxWidth: 340,
   },
   box: {
-    paddingTop: '2rem',
+    marginTop: '2rem',
   },
   link: {
     textDecoration: 'none',
   },
 }));
 
-// サインイン用ページ
 const SignIn: React.FC = () => {
   const classes = useStyles();
-  const history = useHistory();
+  const navigate = useNavigate();
 
   const { setIsSignedIn, setCurrentUser } = useContext(AuthContext);
 
@@ -52,27 +48,19 @@ const SignIn: React.FC = () => {
   const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
 
-    const data: SignInData = {
-      email: email,
-      password: password,
-    };
+    const data: SignInData = { email, password };
 
     try {
       const res = await signIn(data);
-      console.log(res);
-
       if (res.status === 200) {
-        // 成功した場合はCookieに各値を格納
         Cookies.set('_access_token', res.headers['access-token']);
         Cookies.set('_client', res.headers['client']);
         Cookies.set('_uid', res.headers['uid']);
-
         setIsSignedIn(true);
         setCurrentUser(res.data.data);
-
-        history.push('/');
-
-        console.log('Signed in successfully!');
+        navigate('/home');
+        setEmail('');
+        setPassword('');
       } else {
         setAlertMessageOpen(true);
       }
@@ -88,38 +76,29 @@ const SignIn: React.FC = () => {
         <Card className={classes.card}>
           <CardHeader className={classes.header} title="サインイン" />
           <CardContent>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
+            <CommonFormInput
               label="メールアドレス"
               value={email}
-              margin="dense"
-              onChange={(event) => setEmail(event.target.value)}
+              onChange={(e) => setEmail(e.target.value as string)}
             />
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
+            <CommonFormInput
               label="パスワード"
               type="password"
-              placeholder="6文字以上"
               value={password}
-              margin="dense"
-              autoComplete="current-password"
-              onChange={(event) => setPassword(event.target.value)}
+              onChange={(e) => setPassword(e.target.value as string)}
             />
-            <Box className={classes.submitBtn}>
+            <div style={{ textAlign: 'right' }}>
               <Button
                 type="submit"
                 variant="outlined"
                 color="primary"
-                disabled={!email || !password ? true : false}
+                disabled={!email || !password}
+                className={classes.submitBtn}
                 onClick={handleSubmit}
               >
                 送信
               </Button>
-            </Box>
+            </div>
             <Box textAlign="center" className={classes.box}>
               <Typography variant="body2">
                 まだアカウントをお持ちでない方は
@@ -132,7 +111,7 @@ const SignIn: React.FC = () => {
           </CardContent>
         </Card>
       </form>
-      <AlertMessage // エラーが発生した場合はアラートを表示
+      <AlertMessage
         open={alertMessageOpen}
         setOpen={setAlertMessageOpen}
         severity="error"
--- src/components/pages/SignUp.tsx
+++ src/components/pages/SignUp.tsx
@@ -1,23 +1,29 @@
-import React, { useState, useContext } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useContext, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
 
 import { makeStyles, Theme } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import Button from '@material-ui/core/Button';
+import { Grid, Card, CardContent, CardHeader, Button, IconButton, Box } from '@material-ui/core';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import CancelIcon from '@material-ui/icons/Cancel';
 
 import { AuthContext } from 'App';
 import AlertMessage from 'components/utils/AlertMessage';
+import CommonFormInput from 'components/CommonFormInput';
 import { signUp } from 'lib/api/auth';
-import { SignUpData } from 'interfaces/index';
+import { SignUpFormData } from 'interfaces/index';
+import { prefectures } from 'data/prefectures';
+import { genders } from 'data/genders';
 
 const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    marginTop: theme.spacing(6),
+  },
   submitBtn: {
-    paddingTop: theme.spacing(2),
-    textAlign: 'right',
+    marginTop: theme.spacing(1),
     flexGrow: 1,
     textTransform: 'none',
   },
@@ -26,14 +32,29 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   card: {
     padding: theme.spacing(2),
-    maxWidth: 400,
+    maxWidth: 340,
+  },
+  inputFileButton: {
+    textTransform: 'none',
+    color: theme.palette.primary.main,
+  },
+  imageUploadBtn: {
+    textAlign: 'right',
+  },
+  input: {
+    display: 'none',
+  },
+  box: {
+    marginBottom: '1.5rem',
+  },
+  preview: {
+    width: '100%',
   },
 }));
 
-// サインアップ用ページ
 const SignUp: React.FC = () => {
   const classes = useStyles();
-  const histroy = useHistory();
+  const navigate = useNavigate();
 
   const { setIsSignedIn, setCurrentUser } = useContext(AuthContext);
 
@@ -41,35 +62,56 @@ const SignUp: React.FC = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [passwordConfirmation, setPasswordConfirmation] = useState<string>('');
+  const [gender, setGender] = useState<number>();
+  const [prefecture, setPrefecture] = useState<number>();
+  const [birthday, setBirthday] = useState<Date | null>(new Date('2000-01-01T00:00:00'));
+  const [image, setImage] = useState<string>('');
+  const [preview, setPreview] = useState<string>('');
   const [alertMessageOpen, setAlertMessageOpen] = useState<boolean>(false);
+
+  const uploadImage = useCallback((e: any) => {
+    const file = e.target.files[0];
+    setImage(file);
+  }, []);
+
+  const previewImage = useCallback((e: any) => {
+    const file = e.target.files[0];
+    setPreview(window.URL.createObjectURL(file));
+  }, []);
+
+  const createFormData = (): SignUpFormData => {
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('email', email);
+    formData.append('password', password);
+    formData.append('passwordConfirmation', passwordConfirmation);
+    formData.append('gender', String(gender));
+    formData.append('prefecture', String(prefecture));
+    formData.append('birthday', String(birthday));
+    formData.append('image', image);
+    return formData;
+  };
 
   const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-
-    const data: SignUpData = {
-      name: name,
-      email: email,
-      password: password,
-      passwordConfirmation: passwordConfirmation,
-    };
+    const data = createFormData();
 
     try {
       const res = await signUp(data);
-      console.log(res);
-
       if (res.status === 200) {
-        // アカウント作成と同時にサインインさせてしまう
-        // 本来であればメール確認などを挟むべきだが、今回はサンプルなので
         Cookies.set('_access_token', res.headers['access-token']);
         Cookies.set('_client', res.headers['client']);
         Cookies.set('_uid', res.headers['uid']);
-
         setIsSignedIn(true);
         setCurrentUser(res.data.data);
-
-        histroy.push('/');
-
-        console.log('Signed in successfully!');
+        navigate('/home');
+        setName('');
+        setEmail('');
+        setPassword('');
+        setPasswordConfirmation('');
+        setGender(undefined);
+        setPrefecture(undefined);
+        setBirthday(null);
       } else {
         setAlertMessageOpen(true);
       }
@@ -85,52 +127,90 @@ const SignUp: React.FC = () => {
         <Card className={classes.card}>
           <CardHeader className={classes.header} title="サインアップ" />
           <CardContent>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
+            <CommonFormInput
               label="名前"
               value={name}
-              margin="dense"
-              onChange={(event) => setName(event.target.value)}
+              onChange={(e) => setName(e.target.value as string)}
             />
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
+            <CommonFormInput
               label="メールアドレス"
               value={email}
-              margin="dense"
-              onChange={(event) => setEmail(event.target.value)}
+              onChange={(e) => setEmail(e.target.value as string)}
             />
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
+            <CommonFormInput
               label="パスワード"
               type="password"
               value={password}
-              margin="dense"
-              autoComplete="current-password"
-              onChange={(event) => setPassword(event.target.value)}
+              onChange={(e) => setPassword(e.target.value as string)}
             />
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
+            <CommonFormInput
               label="パスワード（確認用）"
               type="password"
               value={passwordConfirmation}
-              margin="dense"
-              autoComplete="current-password"
-              onChange={(event) => setPasswordConfirmation(event.target.value)}
+              onChange={(e) => setPasswordConfirmation(e.target.value as string)}
             />
-            <div className={classes.submitBtn}>
+            <CommonFormInput
+              label="性別"
+              value={gender}
+              onChange={(e) => setGender(e.target.value as number)}
+              options={genders.map((gender, index) => ({ value: index, label: gender }))}
+            />
+            <CommonFormInput
+              label="都道府県"
+              value={prefecture}
+              onChange={(e) => setPrefecture(e.target.value as number)}
+              options={prefectures.map((prefecture, index) => ({
+                value: index + 1,
+                label: prefecture,
+              }))}
+            />
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <Grid container justify="space-around">
+                <KeyboardDatePicker
+                  fullWidth
+                  inputVariant="outlined"
+                  margin="dense"
+                  id="date-picker-dialog"
+                  label="誕生日"
+                  format="MM/dd/yyyy"
+                  value={birthday}
+                  onChange={(date) => setBirthday(date)}
+                  KeyboardButtonProps={{ 'aria-label': 'change date' }}
+                />
+              </Grid>
+            </MuiPickersUtilsProvider>
+            <div className={classes.imageUploadBtn}>
+              <input
+                accept="image/*"
+                className={classes.input}
+                id="icon-button-file"
+                type="file"
+                onChange={(e) => {
+                  uploadImage(e);
+                  previewImage(e);
+                }}
+              />
+              <label htmlFor="icon-button-file">
+                <IconButton color="primary" aria-label="upload picture" component="span">
+                  <PhotoCamera />
+                </IconButton>
+              </label>
+            </div>
+            {preview && (
+              <Box className={classes.box}>
+                <IconButton color="inherit" onClick={() => setPreview('')}>
+                  <CancelIcon />
+                </IconButton>
+                <img src={preview} alt="preview img" className={classes.preview} />
+              </Box>
+            )}
+            <div style={{ textAlign: 'right' }}>
               <Button
                 type="submit"
                 variant="outlined"
                 color="primary"
-                disabled={!name || !email || !password || !passwordConfirmation ? true : false}
+                disabled={!name || !email || !password || !passwordConfirmation}
+                className={classes.submitBtn}
                 onClick={handleSubmit}
               >
                 送信
@@ -139,7 +219,7 @@ const SignUp: React.FC = () => {
           </CardContent>
         </Card>
       </form>
-      <AlertMessage // エラーが発生した場合はアラートを表示
+      <AlertMessage
         open={alertMessageOpen}
         setOpen={setAlertMessageOpen}
         severity="error"
